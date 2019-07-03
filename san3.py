d659efe8964de6dbff6ee03cb8f4221bc88dbc2e
sandyavarma <52460092+sandyavarma@users.noreply.github.com>
2019-07-03 11:39:45 +0530

    Update san3.py

--- san3.py
+++ san3.py
@@ -1,9 +1,9 @@
 Vowels=['a','e','i','o','u']
 Consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 ch=input('')
-if ch is vowel:
-  print('Vowel')
-elif ch is consonant:
+if ch is Vowels:
+  print('Vowels')
+elif ch is Consonants:
   print('Cosonant')
 else:
   print('Invalid')
