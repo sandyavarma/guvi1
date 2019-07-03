75818988113125605913e99519f683ab85b8d0e7
sandyavarma <52460092+sandyavarma@users.noreply.github.com>
2019-07-03 11:22:35 +0530

    Create san3.py

---
+++ san3.py
@@ -0,0 +1,9 @@
+ch=input()
+if(ch=='a'orch=='A'orch=='e'orch=='E'orch=='i'orch=='I'orch=='o'orch=='O'orch=='u'orch=='U')
+else:
+  print("Vowels")
+elif:
+  print("Cosonants")
+else
+  print("Invalid")
+  
