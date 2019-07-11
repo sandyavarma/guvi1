07eae9a9c2263d6d24c0ebc36dcd6d5597b42ac5
sandyavarma <52460092+sandyavarma@users.noreply.github.com>
2019-07-11 16:23:16 +0530

    Update sanp31.py

--- sanp31.py
+++ sanp31.py
@@ -1,7 +1,7 @@
-n1=input().split()
-n2=input().split()
-n3=input().split()
-if(n1[0]==n2[0]==n3[0] or n1[1]==n2[1]==n3[1] or (n1[0]==n1[1] and n2[0]==n2[1] and n3[0]==n3[1])):
+num1=input().split()
+num2=input().split()
+num3=input().split()
+if(num1[0]==num2[0]==num3[0] or num1[1]==num2[1]==num3[1] or (num1[0]==num1[1] and num2[0]==num2[1] and num3[0]==num3[1])):
     print('yes')
 else:
     print('no')
